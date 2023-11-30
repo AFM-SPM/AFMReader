5490f26e3db8a1a88c72431b7baceafd0f0e6669
Neil Shephard <n.shephard@sheffield.ac.uk>
2023-11-30 12:12:43 +0000

    Remove encoding and disable pylint whinging

--- topofileformats/asd.py
+++ topofileformats/asd.py
@@ -163,7 +163,7 @@ def load_asd(file_path: Path | str, channel: str):
     version please either look into the `read_header_file_version_x` functions or print the keys too see what metadata
     is available.
     """
-    with Path.open(file_path, "rb", encoding="UTF-8") as open_file:
+    with Path.open(file_path, "rb", encoding=None) as open_file:  # pylint: disable=W1514
         file_version = read_file_version(open_file)
 
         if file_version == 0:
