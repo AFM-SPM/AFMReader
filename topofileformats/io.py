1063ec13e9d44bc9cdfd894f457d7ad879e9de25
Neil Shephard <n.shephard@sheffield.ac.uk>
2024-04-30 17:39:33 +0100

    docs: Applies numpydoc-validation to existing code base.
    This commit is added to `.git-blame-ignore-revs` so that the blame is not associated with the author.

--- topofileformats/io.py
+++ topofileformats/io.py
@@ -5,11 +5,12 @@ from typing import BinaryIO
 
 
 def read_uint8(open_file: BinaryIO) -> int:
-    """Read an unsigned 8 bit integer from an open binary file.
+    """
+    Read an unsigned 8 bit integer from an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
 
     Returns
@@ -21,11 +22,12 @@ def read_uint8(open_file: BinaryIO) -> int:
 
 
 def read_int8(open_file: BinaryIO) -> int:
-    """Read a signed 8 bit integer from an open binary file.
+    """
+    Read a signed 8 bit integer from an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
 
     Returns
@@ -37,11 +39,12 @@ def read_int8(open_file: BinaryIO) -> int:
 
 
 def read_int16(open_file: BinaryIO) -> int:
-    """Read a signed 16 bit integer from an open binary file.
+    """
+    Read a signed 16 bit integer from an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
 
     Returns
@@ -53,11 +56,12 @@ def read_int16(open_file: BinaryIO) -> int:
 
 
 def read_int32(open_file: BinaryIO) -> int:
-    """Read a signed 32 bit integer from an open binary file.
+    """
+    Read a signed 32 bit integer from an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
 
     Returns
@@ -69,11 +73,12 @@ def read_int32(open_file: BinaryIO) -> int:
 
 
 def read_uint32(open_file: BinaryIO) -> int:
-    """Read an unsigned 32 bit integer from an open binary file.
+    """
+    Read an unsigned 32 bit integer from an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
 
     Returns
@@ -85,11 +90,12 @@ def read_uint32(open_file: BinaryIO) -> int:
 
 
 def read_hex_u32(open_file: BinaryIO) -> str:
-    """Read a hex encoded unsigned 32 bit integer value from an open binary file.
+    """
+    Read a hex encoded unsigned 32 bit integer value from an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
 
     Returns
@@ -101,101 +107,106 @@ def read_hex_u32(open_file: BinaryIO) -> str:
 
 
 def read_float(open_file: BinaryIO) -> float:
-    """Read a float from an open binary file.
+    """
+    Read a float from an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
 
     Returns
     -------
     float
-    Float decoded value.
+        Float decoded value.
     """
     return struct.unpack("f", open_file.read(4))[0]
 
 
 def read_bool(open_file: BinaryIO) -> bool:
-    """Read a boolean from an open binary file.
+    """
+    Read a boolean from an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
 
     Returns
     -------
     bool
-        Boolean decoded value
+        Boolean decoded value.
     """
     return bool(int.from_bytes(open_file.read(1), byteorder="little"))
 
 
 def read_double(open_file: BinaryIO) -> float:
-    """Read an 8 byte double from an open binary file.
+    """
+    Read an 8 byte double from an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
 
     Returns
     -------
     float
-        Float decoded from the double value
+        Float decoded from the double value.
     """
     return struct.unpack("d", open_file.read(8))[0]
 
 
 def read_ascii(open_file: BinaryIO, length_bytes: int = 1) -> str:
-    """Read an ascii string of defined length from an open binary file.
+    """
+    Read an ASCII string of defined length from an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
-    length_bytes: int
-        Length of the ascii string in bytes that should be read. Default: 1 byte (1 character).
+    length_bytes : int
+        Length of the ASCII string in bytes that should be read. Default: 1 byte (1 character).
 
     Returns
     -------
     str
-        Ascii text decoded from file.
+        ASCII text decoded from file.
     """
     return open_file.read(length_bytes).decode("ascii")
 
 
 def read_null_separated_utf8(open_file: BinaryIO, length_bytes: int = 2) -> str:
-    r"""Read an ASCII string of defined length from an open binary file.
+    r"""
+    Read an ASCII string of defined length from an open binary file.
 
     Each character is separated by a null byte. This encoding is known as UTF-16LE (Little Endian).
     Eg: b'\x74\x00\x6f\x00\x70\x00\x6f' would decode to 'topo' in this format.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
-    length_bytes: int
-        Length of the ascii string in bytes that should be read. Default: 2 bytes
-        (1 UTF-16LE character)
+    length_bytes : int
+        Length of the ASCII string in bytes that should be read. Default: 2 bytes (1 UTF-16LE character).
 
     Returns
     -------
     str
-        Ascii text decoded from file.
+        ASCII text decoded from file.
     """
     return open_file.read(length_bytes).replace(b"\x00", b"").decode("ascii")
 
 
 def skip_bytes(open_file: BinaryIO, length_bytes: int = 1) -> bytes:
-    """Skip a specified number of bytes when reading an open binary file.
+    """
+    Skip a specified number of bytes when reading an open binary file.
 
     Parameters
     ----------
-    open_file: BinaryIO
+    open_file : BinaryIO
         An open binary file object.
-    length_bytes: int
+    length_bytes : int
         Number of bytes to skip.
 
     Returns
