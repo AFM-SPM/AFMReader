f4f6c9d2d9ad792512ddb1f3aaad4e9ca48492c1
Neil Shephard <n.shephard@sheffield.ac.uk>
2024-12-11 15:16:57 +0000

    tests(spm): Adds test for spm_pixel_to_nm_scaling()
    Closes #86

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+"""Fixtures for testing."""
+
+from pathlib import Path
+
+import pySPM
+import pytest
+
+BASE_DIR = Path.cwd()
+RESOURCES = BASE_DIR / "tests" / "resources"
+
+
+@pytest.fixture()
+def spm_channel_data() -> pySPM.SPM.SPM_image:
+    """Instantiate channel data from a LoadScans object."""
+    scan = pySPM.Bruker(RESOURCES / "sample_0.spm")
+    return scan.get_channel("Height")
--- tests/test_spm.py
+++ tests/test_spm.py
@@ -1,14 +1,18 @@
 """Test the loading of spm files."""
 
 from pathlib import Path
-import pytest
+from unittest.mock import patch
 
 import numpy as np
+import pySPM
+import pytest
 
-from AFMReader.spm import load_spm
+from AFMReader import spm
 
 BASE_DIR = Path.cwd()
 RESOURCES = BASE_DIR / "tests" / "resources"
+
+# pylint: disable=too-many-positional-arguments
 
 
 @pytest.mark.parametrize(
@@ -32,10 +36,37 @@ def test_load_spm(
     result_pixel_to_nm_scaling = float
 
     file_path = RESOURCES / file_name
-    result_image, result_pixel_to_nm_scaling = load_spm(file_path, channel=channel)
+    result_image, result_pixel_to_nm_scaling = spm.load_spm(file_path, channel=channel)
 
     assert result_pixel_to_nm_scaling == pixel_to_nm_scaling
     assert isinstance(result_image, np.ndarray)
     assert result_image.shape == image_shape
     assert result_image.dtype == image_dtype
     assert result_image.sum() == image_sum
+
+
+@patch("pySPM.SPM.SPM_image.pxs")
+@pytest.mark.parametrize(
+    ("filename", "unit", "x", "y", "expected_px2nm"),
+    [
+        pytest.param("square_mm", "mm", 0.01, 0.01, 10000, id="mm units; square"),
+        pytest.param("square_um", "um", 1.5, 1.5, 1500, id="um units; square"),
+        pytest.param("square_nm", "nm", 50, 50, 50, id="nm units; square"),
+        pytest.param("square_pm", "pm", 233, 233, 0.233, id="pm units; square"),
+        pytest.param("rectangle_thin_pm", "pm", 1, 512, 0.001, id="pm units; rectangular (thin)"),
+        pytest.param("rectangle_tall_pm", "pm", 512, 1, 0.512, id="pm units; rectangular (tall)"),
+    ],
+)
+def test__spm_pixel_to_nm_scaling(
+    mock_pxs,
+    spm_channel_data: pySPM.SPM.SPM_image,
+    filename: str,
+    unit: str,
+    x: int,
+    y: int,
+    expected_px2nm: float,
+) -> None:
+    """Test extraction of pixels to nanometer scaling."""
+    mock_pxs.return_value = [(x, unit), (y, unit)]  # issue is that pxs is a func that returns the data
+    result = spm.spm_pixel_to_nm_scaling(filename, spm_channel_data)
+    assert result == expected_px2nm
