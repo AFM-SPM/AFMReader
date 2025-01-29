0135ec9e4e017aa6ecb4467fb56381a82105ba3d
Neil Shephard <n.shephard@sheffield.ac.uk>
2025-01-29 09:39:16 +0000

    featire: Ensure img_path is of type Path
    Closes #106
    
    Ensures that if fileversion >= 0.2 is being loaded that the `data["img_path"]` variable is of type `Path`.
    
    Adds to the test suite a 0.2 sample file to check this (and expands on the keys of the dictionary).

--- tests/test_topostats.py
+++ tests/test_topostats.py
@@ -19,9 +19,36 @@ RESOURCES = BASE_DIR / "tests" / "resources"
             0.1,
             (64, 64),
             1.97601171875,
-            {"topostats_file_version", "image", "pixel_to_nm_scaling"},
+            {
+                "pixel_to_nm_scaling",
+                "image",
+                "topostats_file_version",
+            },
             112069.51332503435,
-            id="version",
+            id="version 0.1",
+        ),
+        pytest.param(
+            "sample_0_2.topostats",
+            0.2,
+            (64, 64),
+            1.97601171875,
+            {
+                "disordered_traces",
+                "filename",
+                "grain_curvature_stats",
+                "grain_masks",
+                "height_profiles",
+                "image",
+                "image_original",
+                "img_path",
+                "nodestats",
+                "ordered_traces",
+                "pixel_to_nm_scaling",
+                "splining",
+                "topostats_file_version",
+            },
+            1176.601500471239,
+            id="version 0.2",
         ),
     ],
 )
@@ -47,3 +74,5 @@ def test_load_topostats(
     assert set(result_data.keys()) == data_keys  # type: ignore
     assert result_data["topostats_file_version"] == topostats_file_version
     assert result_image.sum() == image_sum
+    if topostats_file_version >= 0.2:
+        assert isinstance(result_data["img_path"], Path)
