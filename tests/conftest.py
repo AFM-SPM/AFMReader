89ba87bf74a963bac37a7b3207caac5087bb4aee
Max Gamill <mcgamill1@sheffield.ac.uk>
2025-04-08 13:33:35 +0100

    ensure loguru works with caplog via a logging sink

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,27 @@
 """Fixtures for testing."""
 
 from pathlib import Path
+import logging
 
 import pySPM
 import pytest
+from _pytest.logging import caplog as _caplog
+
+from AFMReader.logging import logger
 
 BASE_DIR = Path.cwd()
 RESOURCES = BASE_DIR / "tests" / "resources"
+
+
+@pytest.fixture()
+def caplog(_caplog):
+    class PropogateHandler(logging.Handler):
+        def emit(self, record):
+            logging.getLogger(record.name).handle(record)
+
+    handler_id = logger.add(PropogateHandler(), format="{message}")
+    yield _caplog
+    logger.remove(handler_id)
 
 
 @pytest.fixture()
