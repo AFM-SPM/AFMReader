c4a9ac7a3f2b176b609eb1d70b147864b968a58a
Max Gamill <mcgamill1@sheffield.ac.uk>
2025-04-08 13:33:11 +0100

    Add tests to check terminal message

--- tests/test_spm.py
+++ tests/test_spm.py
@@ -6,6 +6,7 @@ from unittest.mock import patch, MagicMock
 import numpy as np
 import pySPM
 import pytest
+import regex as re
 
 from AFMReader import spm
 
@@ -76,3 +77,37 @@ def test_load_spm_file_not_found() -> None:
     """Ensure FileNotFound error is raised."""
     with pytest.raises(FileNotFoundError):
         spm.load_spm("nonexistant_file.spm", channel="TP")
+
+
+@pytest.mark.parametrize(
+    ("channel", "message", "error"),
+    [
+        pytest.param("Height", "Extracted channel Height", False, id="Height found"),
+        pytest.param("Height Sensor", "Extracted channel Height Sensor", False, id="Height Sensor found"),
+        pytest.param("Peak Force Error", "Extracted channel Peak Force Error", False, id="Peak Force Error found"),
+        pytest.param("DMTModulus", "Extracted channel DMTModulus", False, id="DMTModulus found"),
+        pytest.param("LogDMTModulus", "Extracted channel LogDMTModulus", False, id="LogDMTModulus found"),
+        pytest.param("Adhesion", "Extracted channel Adhesion", False, id="Adhesion found"),
+        pytest.param("Deformation", "Extracted channel Deformation", False, id="Deformation found"),
+        pytest.param("Dissipation", "Extracted channel Dissipation", False, id="Dissipation found"),
+        pytest.param(
+            "Might",
+            "Might not in .spm channel list: ['Height Sensor', 'Peak Force Error', 'DMTModulus', 'LogDMTModulus', 'Adhesion', 'Deformation', 'Dissipation', 'Height']",
+            True,
+            id="Might not found",
+        ),
+    ],
+)
+def test_load_spm_channel_not_found(
+    caplog: pytest.LogCaptureFixture,
+    channel: str,
+    message: str,
+    error: bool,
+) -> None:
+    """Test the failure and success messages are correct."""
+    if error:
+        with pytest.raises(ValueError, match=re.escape(message)):
+            spm.load_spm(RESOURCES / "sample_0.spm", channel)
+    else:
+        spm.load_spm(RESOURCES / "sample_0.spm", channel)
+    assert message in caplog.text
