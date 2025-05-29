0450b431035fff1177b2b3fa08ae3a7b24d29515
Daniel Eddie Rollins <dan.rollins1@gmail.com>
2025-05-29 12:58:14 +0100

    update general loader, Readme and examples

--- AFMReader/general_loader.py
+++ AFMReader/general_loader.py
@@ -65,7 +65,7 @@ class LoadFile:
             elif self.suffix == ".spm":
                 image, pixel_to_nanometre_scaling_factor = spm.load_spm(self.filepath, self.channel)
             elif self.suffix == ".h5-jpk":
-                image, pixel_to_nanometre_scaling_factor = h5_jpk.load_h5jpk(self.filepath, self.channel)
+                image, pixel_to_nanometre_scaling_factor, _ = h5_jpk.load_h5jpk(self.filepath, self.channel)
             elif self.suffix == ".stp":
                 image, pixel_to_nanometre_scaling_factor = stp.load_stp(self.filepath)
             elif self.suffix == ".top":
