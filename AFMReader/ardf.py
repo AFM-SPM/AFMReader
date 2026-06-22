ced35ee494283e0c8dc4db070f270f5292cb6760
ahobbs7 <ahobbs834@gmail.com>
2026-06-22 08:41:39 +0100

    docs: updating copyright notice for ardf reader

--- AFMReader/ardf.py
+++ AFMReader/ardf.py
@@ -4,17 +4,16 @@
 # Copyright (C) Richard J. Sheridan
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# See <https://www.gnu.org/licenses/> for a copy of the GNU General Public License.
 #
 # This file includes code copied and adapted from Magic AFM
 # (https://github.com/richardsheridan/magic-afm).
